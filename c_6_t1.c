b5e58e275a83b7953da2b3b8b921fc38951ee15b
Gyuwonn <128655469+Gyuwonn@users.noreply.github.com>
2023-04-27 15:26:23 +0900

    Add files via upload

---
+++ c_6_t1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+  double money = 1.0;
+
+  for(int i = 2; i <= 30; i++)
+  {
+    money *= 2.0;
+    printf("%d일날 현재 급액=%lf\n",i, money);
+  }
+  return 0;
+}
